a4d295e1b69cdc841ea5de4d04887f9ec8bd11c9
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-27 19:24:08 +0000

    made changes to the files

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-"""
-ETL-Query script
-"""
-
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-# Extract
-print("Extracting data...")
-extract()
+def main():
+    parser = argparse.ArgumentParser(description="Extract, transform, and query data.")
+    parser.add_argument('--extract', action='store_true', help='Perform data extraction')
+    parser.add_argument('--load', action='store_true', help='Perform data transformation and loading')
+    parser.add_argument('--query', action='store_true', help='Perform data querying')
 
-# Transform and load
-print("Transforming data...")
-load()
+    args = parser.parse_args()
 
-# Query
-print("Querying data...")
-query()
+    if args.extract:
+        print("Extracting data...")
+        extract()
+
+    if args.load:
+        print("Transforming data...")
+        load()
+
+    if args.query:
+        print("Querying data...")
+        query()
+
+if __name__ == '__main__':
+    main()
