6f9cdeb40edeba7cf6eac28e7d6fac5048341461
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-29 19:26:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from mylib.query import query
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Extract, transform, and query data.")
+    parser = argparse.ArgumentParser(description="Extract, transform, and query data, generate and push")
     parser.add_argument(
         "--extract", action="store_true", help="Perform data extraction"
     )
     parser.add_argument(
         "--load", action="store_true", help="Perform data transformation and loading"
     )
-    parser.add_argument("--query", action="store_true", help="Perform data querying")
+    parser.add_argument(
+        "--query", action="store_true", help="Perform data querying"
+    )
+    parser.add_argument(
+        '--generate_and_push', action='store_true', help="Generate and push data"
+    )
 
     args = parser.parse_args()
 
@@ -28,6 +33,10 @@ def main():
         print("Querying data...")
         query()
 
+    if args.generate_and_push:
+    # Add your code for generating and pushing data here
+        print("Generating and pushing data...")
+        generate_and_push()
 
 if __name__ == "__main__":
     main()
