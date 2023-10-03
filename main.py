f199686b498b8ec7a3592afbf91a9683648abb9a
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-02 22:23:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
-import argparse
+"""
+ETL-Query script
+"""
+import fire
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Extract, transform, and query data")
-    parser.add_argument(
-        "--extract", action="store_true", help="Perform data extraction"
-    )
-    parser.add_argument(
-        "--load", action="store_true", help="Perform data transformation and loading"
-    )
-    parser.add_argument("--query", action="store_true", help="Perform data querying")
+def main(query_string):
+    # Extract
+    print("Extracting data...")
+    extract()
 
-    args = parser.parse_args()
+    # Transform and load
+    print("Transforming data...")
+    load()
 
-    if args.extract:
-        print("Extracting data...")
-        extract()
-
-    if args.load:
-        print("Transforming data...")
-        load()
-
-    if args.query:
-        print("Querying data...")
-        query()
+    # Query
+    print("Querying data...")
+    query(query_string)
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(main)
+
+# import argparse
+# from mylib.extract import extract
+# from mylib.transform_load import load
+# from mylib.query import query
+
+
+# def main():
+#     parser = argparse.ArgumentParser(description="Extract, transform, and query data")
+#     parser.add_argument(
+#         "--extract", action="store_true", help="Perform data extraction"
+#     )
+#     parser.add_argument(
+#         "--load", action="store_true", help="Perform data transformation and loading"
+#     )
+#     parser.add_argument("--query", action="store_true", help="Perform data querying")
+
+#     args = parser.parse_args()
+
+#     if args.extract:
+#         print("Extracting data...")
+#         extract()
+
+#     if args.load:
+#         print("Transforming data...")
+#         load()
+
+#     if args.query:
+#         print("Querying data...")
+#         query()
+
+
+# if __name__ == "__main__":
+#     main()
