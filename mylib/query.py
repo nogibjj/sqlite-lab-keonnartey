13374d87e0a3875462c78e02177c754a4d050451
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-02 22:13:00 -0700

    new query.py

--- mylib/query.py
+++ mylib/query.py
@@ -3,12 +3,18 @@
 import sqlite3
 
 
-def query():
+def query(query):
     """Query the database for the top 5 rows of the DiabetesDB table"""
     conn = sqlite3.connect("DiabetesDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM DiabetesDB LIMIT 5")
-    print("Top 5 rows of the DiabetesDB table:")
-    print(cursor.fetchall())
+    cursor.execute(query)
+    q_result = cursor.fetchall()
+    print(q_result)
     conn.close()
-    return "Success"
+    return q_result
+    
+    # cursor.execute("SELECT * FROM DiabetesDB LIMIT 5")
+    # print("Top 5 rows of the DiabetesDB table:")
+    # print(cursor.fetchall())
+    # conn.close()
+    # return "Success"
