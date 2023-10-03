1367b891f932d0a1ddc7243f6531ff36658b0ad7
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-02 22:47:28 -0700

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -8,35 +8,35 @@ def extract(
 ):
     
 
-    """Extract a url to a file path"""
-    with requests.get(url) as r:
-        with open(file_path, "wb") as f:
-            f.write(r.content)
-    return file_path
-#     # Ensure the directory exists
-#     os.makedirs(directory, exist_ok=True)
+    # """Extract a url to a file path"""
+    # with requests.get(url) as r:
+    #     with open(file_path, "wb") as f:
+    #         f.write(r.content)
+    # return file_path
+    # # Ensure the directory exists
+    os.makedirs(directory, exist_ok=True)
     
-#     # Join the directory and file name to get the full path
-#     full_file_path = os.path.join(directory, 'Diabetes.csv')
+    # Join the directory and file name to get the full path
+    full_file_path = os.path.join(directory, 'Diabetes.csv')
     
-#     # Check if the file already exists
-#     if os.path.exists(full_file_path):
-#         print(f"File '{full_file_path}' already exists.")
-#         return full_file_path
+    # Check if the file already exists
+    if os.path.exists(full_file_path):
+        print(f"File '{full_file_path}' already exists.")
+        return full_file_path
 
-#     # Fetch the content from the URL
-#     response = requests.get(url)
+    # Fetch the content from the URL
+    response = requests.get(url)
 
-#     if response.status_code == 200:
-#         # Write the content to the file
-#         with open(full_file_path, "wb") as f:
-#             f.write(response.content)
-#         print(f"File '{full_file_path}' extracted successfully.")
-#     else:
-#         print(f"Failed to fetch content from URL. Status code:
-# was on top {response.status_code}")
+    if response.status_code == 200:
+        # Write the content to the file
+        with open(full_file_path, "wb") as f:
+            f.write(response.content)
+        print(f"File '{full_file_path}' extracted successfully.")
+    else:
+        print(f"Failed to fetch content from URL. Status code:
+was on top {response.status_code}")
     
-#     return full_file_path
+    return full_file_path
 
-# # Example usage
-# extract()
+# Example usage
+extract()
