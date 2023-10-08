b9d6f7a42998d4ca9e1c0156d769b8fdb8ae727c
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-08 15:31:26 -0700

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -1,34 +1,20 @@
-"""Query the database from a db connection to Azure Databricks"""
-import os
-from databricks import sql
-from dotenv import load_dotenv
+"""Query the database"""
+
+import sqlite3
 
 
-# Define a global variable for the log file
-LOG_FILE = "query_log.md"
-
-
-def log_query(query, result="none"):
-    """adds to a query markdown file"""
-    with open(LOG_FILE, "a") as file:
-        file.write(f"```sql\n{query}\n```\n\n")
-        file.write(f"```response from databricks\n{result}\n```\n\n")
-
-
-def general_query(query):
-    """runs a query a user inputs"""
-
-    load_dotenv()
-    server_h = os.getenv("SERVER_HOSTNAME")
-    access_token = os.getenv("ACCESS_TOKEN")
-    http_path = os.getenv("HTTP_PATH")
-    with sql.connect(
-        server_hostname=server_h,
-        http_path=http_path,
-        access_token=access_token,
-    ) as connection:
-        c = connection.cursor()
-        c.execute(query)
-        result = c.fetchall()
-    c.close()
-    log_query(f"{query}", result)
+def query(query):
+    """Query the database for the top 5 rows of the DiabetesDB table"""
+    conn = sqlite3.connect("DiabetesDB.db")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    q_result = cursor.fetchall()
+    print(q_result)
+    conn.close()
+    return q_result
+    
+    # cursor.execute("SELECT * FROM DiabetesDB LIMIT 5")
+    # print("Top 5 rows of the DiabetesDB table:")
+    # print(cursor.fetchall())
+    # conn.close()
+    # return "Success"
